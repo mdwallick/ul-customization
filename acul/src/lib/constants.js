28d886cead785a7b8c404fbbdb81dab43130a31c
Mike Wallick <mike@wallick.net>
2025-03-30 18:08:30 -0500

    updates local settings

--- acul/src/lib/constants.js
+++ acul/src/lib/constants.js
@@ -1,6 +1,5 @@
 // Base URL for Assets (Local or Remote)
-export const BASE_URL = "http://localhost:3000";
-// export const BASE_URL = "https://bucket-name.s3.us-east-2.amazonaws.com";
+export const BASE_URL = "http://localhost:4000"
 
 // App Homepage URL
-export const APP_URL = "#";
+export const APP_URL = "http://localhost:3000"
