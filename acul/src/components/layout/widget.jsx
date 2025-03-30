22dd2afbdab63d647680b1e38d13b2d9db2825d8
Mike Wallick <mike@wallick.net>
2025-03-30 18:10:02 -0500

    Updates styles

--- acul/src/components/layout/widget.jsx
+++ acul/src/components/layout/widget.jsx
@@ -4,17 +4,17 @@ import { getScreenComponent } from "@/screens";
 import { Card } from "@/components/ui/card";
 
 export default function Widget() {
-  const screen = getCurrentScreen() || "";
+    const screen = getCurrentScreen() || "";
 
-  return (
-    <div className="flex-1 w-full mx-auto max-w-7x">
-      <Card className="w-[512px] p-4 shadow-lg my-12 mx-auto">
-        <img
-          src={`${BASE_URL}/images/auth0-icon-onlight.svg`}
-          className="block w-12 mb-6 ml-auto mr-auto"
-        />
-        {getScreenComponent(screen)}
-      </Card>
-    </div>
-  );
+    return (
+        <div className="flex-1 w-full mx-auto max-w-7x">
+            <Card className="w-[512px] p-4 shadow-lg my-12 mx-auto">
+                <img
+                    src={`${BASE_URL}/images/WOF_s42_onion_logo_rev.png`}
+                    className="block w-12 mb-6 ml-auto mr-auto"
+                />
+                {getScreenComponent(screen)}
+            </Card>
+        </div>
+    );
 }
