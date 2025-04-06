698a9ff887acaf4c30e23f0d8ecbd7efb19f7503
Mike Wallick <mike@wallick.net>
2025-04-06 16:36:01 -0500

    adds screen support for login-password

--- acul/ul-config/auth0_cli_commands.sh
+++ acul/ul-config/auth0_cli_commands.sh
@@ -15,6 +15,7 @@ auth0 ul customize -r advanced -p {PROMPT} -s {SCREEN} -f {CONFIG_FILE}
 
 # screens that have been implemented
 auth0 ul customize -r advanced -p "login-id" -s "login-id" -f ./screen_configs/login-id.json
+auth0 ul customize -r advanced -p "login-password" -s "login-password" -f ./screen_configs/login-password.json
 auth0 ul customize -r advanced -p "login-passwordless" -s "login-passwordless-email-code" -f ./screen_configs/login-passwordless-email-code.json
 auth0 ul customize -r advanced -p "login-passwordless" -s "login-passwordless-sms-otp" -f ./screen_configs/login-passwordless-sms-otp.json
 
