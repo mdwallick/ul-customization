af55f2f80cf5aba50e6d8cf36c02fe9d641950c9
Mike Wallick <mike@wallick.net>
2025-04-06 16:25:05 -0500

    adds branding for WoF

--- acul/src/components/layout/widget.jsx
+++ acul/src/components/layout/widget.jsx
@@ -10,7 +10,7 @@ export default function Widget() {
         <div className="flex min-h-screen items-center justify-center">
             <Card className="w-full max-w-lg p-8 bg-white rounded-lg shadow-md">
                 <img
-                    src={`${BASE_URL}/images/auth0-icon-onlight.svg`}
+                    src={`${BASE_URL}/images/WOF_s42_onion_logo_rev.png`}
                     className="block w-12 mb-6 ml-auto mr-auto"
                 />
                 {getScreenComponent(screen)}
--- acul/src/lib/constants.js
+++ acul/src/lib/constants.js
@@ -1,7 +1,5 @@
 // Base URL for Assets (Local or Remote)
-export const BASE_URL =
-  "https://storage.googleapis.com/thorax-studios-acul/acul-demo"
+export const BASE_URL = "https://storage.googleapis.com/thorax-studios-acul/wof"
 
 // App Homepage URL
-//export const APP_URL = "http://localhost:3000"
 export const APP_URL = "https://acul-demo-71214068973.us-central1.run.app"
