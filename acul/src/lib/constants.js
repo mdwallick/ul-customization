2b2a129320737ca9531b8f13d6ffe40b181f122c
Mike Wallick <mike@wallick.net>
2025-04-22 13:59:58 -0500

    updates base url for ACUL assets

--- acul/src/lib/constants.js
+++ acul/src/lib/constants.js
@@ -1,7 +1,8 @@
 // Base URL for Assets (Local or Remote)
-export const BASE_URL =
-  "https://storage.googleapis.com/thorax-studios-acul/acul-demo"
+export const BASE_URL = "https://storage.googleapis.com/acul/acul-demo"
+
+// export const BASE_URL = "http://localhost:4000"
 
 // App Homepage URL
-//export const APP_URL = "http://localhost:3000"
-export const APP_URL = "https://acul-demo-71214068973.us-central1.run.app"
+export const APP_URL = "http://localhost:3000"
+// export const APP_URL = "https://acul-demo-71214068973.us-central1.run.app"
