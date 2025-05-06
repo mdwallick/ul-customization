7a5200eac9499693a2d6819e5d99511df5cfd5db
Mike Wallick <mike@wallick.net>
2025-05-06 10:57:24 -0500

    update UL reset commands

--- acul/ul-config/reset-ul.sh
+++ acul/ul-config/reset-ul.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-auth0 ul switch -r standard -p "login-id" -s "login-id"
-auth0 ul switch -r standard -p "login-password" -s "login-password"
-auth0 ul switch -r standard -p "login-passwordless" -s "login-passwordless-email-code"
-auth0 ul switch -r standard -p "login-passwordless" -s "login-passwordless-sms-otp"
+# auth0 ul switch -r standard -p "login-id" -s "login-id"
+# auth0 ul switch -r standard -p "login-password" -s "login-password"
+# auth0 ul switch -r standard -p "login-passwordless" -s "login-passwordless-email-code"
+# auth0 ul switch -r standard -p "login-passwordless" -s "login-passwordless-sms-otp"
+
+auth0 ul customize -r advanced -p "login-id" -s "login-id" -f ./screen_configs/reset-to-standard.json
+auth0 ul customize -r advanced -p "login-password" -s "login-password" -f ./screen_configs/reset-to-standard.json
+auth0 ul customize -r advanced -p "login-passwordless" -s "login-passwordless-email-code" -f ./screen_configs/reset-to-standard.json
+auth0 ul customize -r advanced -p "login-passwordless" -s "login-passwordless-sms-otp" -f ./screen_configs/reset-to-standard.json
